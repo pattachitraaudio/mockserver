4bde0f73e63dae3339ab45c4192976b3d5f9fbc6
Pattachitra Audio <pattachitra.audio@gmail.com>
2025-11-15 19:19:01 +0530

    Added env variable for elevenLabs api

--- sources/api/elevenLabs/index.ts
+++ sources/api/elevenLabs/index.ts
@@ -1,9 +1,16 @@
 import { Hono } from "hono";
 import { user } from "./user/index.ts";
 
-const host = "api.elevenlabs.io";
+// const host = "api.elevenlabs.io";
+const hostname = Deno.env.get("ELEVEN_LABS_API_HOSTNAME");
+
+if (hostname == null) {
+    console.error("env variable `ELEVEN_LABS_API_HOSTNAME cannot be null`");
+    Deno.exit(-2);
+}
+
 const elevenLabs = new Hono();
 
-elevenLabs.route(`/${host}`, user);
+elevenLabs.route(`/${hostname}`, user);
 
 export { elevenLabs };
